a43e06c0a05bb4441e941bc45116319a07d739d0
DESKTOP-BUJUNA0\EdTech <haynesma@gmail.com>
2018-06-22 09:13:53 -0500

    More updates to template, added grid-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatDialogModule } from "@angular/material/dialog";
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatButtonModule } from "@angular/material";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 //Uncomment if using animations
 //import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { SharedModule } from "./shared/shared.module";
-import { AppComponent } from './app.component';
-import { OverviewComponent } from './overview/overview/overview.component';
-import { FooterComponent } from './footer/footer/footer.component';
-import { HeaderComponent } from './header/header/header.component';
-import { FeaturesComponent } from './features/features/features.component';
-import { Features2Component } from './features2/features2.component';
-import { Features3Component } from './features3/features3.component';
-// import { MatDialog } from '@angular/material';
+import { AppComponent } from "./app.component";
+import { OverviewComponent } from "./overview/overview/overview.component";
+import { FooterComponent } from "./footer/footer/footer.component";
+import { HeaderComponent } from "./header/header/header.component";
+import { FeaturesComponent } from "./features/features/features.component";
+import { Features2Component } from "./features2/features2.component";
+import { Features3Component } from "./features3/features3.component";
 
 @NgModule({
   declarations: [
@@ -25,26 +26,22 @@ import { Features3Component } from './features3/features3.component';
     HeaderComponent,
     FeaturesComponent,
     Features2Component,
-    Features3Component,
+    Features3Component
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     SharedModule,
     MatDialogModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [
     //{provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {hasBackdrop: false}}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
-//export class YourDialog {
-  //constructor(public dialogRef: MatDialogRef<YourDialog>) {}
+export class AppModule {}
 
-//  closeDialog () {
-  //  this.dialogRef.close('Pizza!');
- // }
 //}
-
